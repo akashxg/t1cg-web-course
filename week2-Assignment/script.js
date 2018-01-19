379f88cdafda7efe7d7805c35f605f3bdcc058f0
Akash Gheewala <akashxg@gmail.com>
2018-01-19 02:30:35 -0500

    week 2 assignment WIP

--- week2-Assignment/script.js
+++ week2-Assignment/script.js
@@ -1,1 +1,43 @@
+const videoButton = document.getElementById("videoButton");
+const seagalPic = document.getElementById("seagalPic");
+//create celebrityVideo, table Button, movieTable, titleInput, yearInput and trivia variables for DOM manipulation using above code as reference
 
+
+triviaFunc = () => {
+  alert("Please enter the letter for the corresponding answer.");
+  const value1 = prompt("What was Steven Seagal's first movie?   a: Above the Law, b: Under Siege, c: The Glimmer Man");
+  if(value1 === "a"){
+    alert("You got it right!");
+  } else {
+    alert("Incorrect, Please try again.")
+    triviaFunc();
+  }
+
+  const value2 = prompt("How old is Steven Seagal?   a: 57, b: 72, c: 65" );
+  //use value collected from prompt, if else statements, operators, and alerts to handle trivia question
+
+  const value3 = prompt("What 2 martial arts forms does Seagal know? a: Akido, b: Muay Thai, c: Boxing, d: Judo");
+  //use value collected from prompt, if else statements, operators, and alerts to handle trivia question
+
+}
+
+trivia.addEventListener("click", function(){
+//call triviaFunc
+});
+
+videoButton.addEventListener("click", function(){
+//toggle celebrityPic and celebrityVideo classes with button functionality
+});
+
+tableButton.addEventListener("click", function(){
+  const row = movieTable.insertRow(1);
+  const cell1 = row.insertCell(0);
+  // create variable to insert new cell similar to code above
+  cell1.innerHTML = titleInput.value;
+  // add yearInput value to cell you just created
+  //call resetInputValues function;
+});
+
+resetInputValues = () => {
+  //define function to reset input values
+};
